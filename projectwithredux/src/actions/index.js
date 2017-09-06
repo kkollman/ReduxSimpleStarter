8a5a5c4c1ef9022438a222853271c7b907708b91
Kamil Kollman <kamil.kollman@venturedevs.com>
2017-09-06 22:31:27 +0200

    finished section

--- projectwithredux/src/actions/index.js
+++ projectwithredux/src/actions/index.js
@@ -1,5 +1,5 @@
 export function selectBook(book) {
-//selectBook is an Action Creastor, it needs to return an action - an object with type property
+//selectBook is an Action Creator, it needs to return an action - an object with type property
   return {
     type: 'BOOK_SELECTED',
     payload: book
--- projectwithredux/src/reducers/reducer_books.js
+++ projectwithredux/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    {title: 'Adventures of Augie March'},
-    {title: 'Do Androids dream of electric sheep?'},
-    {title: 'Lolita'},
-    {title: 'Henderson, the Rain King'},
+    {title: 'Adventures of Augie March', pages: 300},
+    {title: 'Do Androids dream of electric sheep?', pages: 404},
+    {title: 'Lolita', pages: 279},
+    {title: 'Henderson, the Rain King', pages: 330},
   ]
 }
